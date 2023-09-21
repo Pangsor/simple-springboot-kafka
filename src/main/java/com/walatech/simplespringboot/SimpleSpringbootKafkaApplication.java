f3d4c915d935048b9ad8963e80b42e951fd47c1c
Pangsor <pangsor89@gmail.com>
2023-09-21 09:48:06 +0700

    first commit

---
+++ src/main/java/com/walatech/simplespringboot/SimpleSpringbootKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.walatech.simplespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringbootKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringbootKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/walatech/simplespringboot/SimpleSpringbootKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walatech.simplespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringbootKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
